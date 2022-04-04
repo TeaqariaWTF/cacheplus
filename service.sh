9299df44ddf748116ffad5a30206a18ca8575e6f
Taka <takeru.kageyuki@gmail.com>
2022-04-04 18:29:57 +0700

    Update service.sh

--- service.sh
+++ service.sh
@@ -2,5 +2,5 @@
 MODDIR=${0%/*}
 
 # Cache Cleaner (clear cache of all apps on every boot)
-find /data/data/*/cache/* -delete
-find /sdcard/Android/data/*/cache/* -delete
+find /data/data/*/cache/* -delete &>/dev/null
+find /sdcard/Android/data/*/cache/* -delete &>/dev/null
