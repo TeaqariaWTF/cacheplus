cb3f1d7b8c8d60655584c4712c1048a0369fa7a2
Taka <takeru.kageyuki@gmail.com>
2022-04-04 19:31:55 +0700

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -2,34 +2,35 @@ SKIPUNZIP=1
 
 PRINT() {
     ui_print "Cache+ (Magisk Module)"
+    sleep 1.5
     ui_print "• A module that allows your device"
     ui_print "• To clear cache of all apps on every boot."
     ui_print ""
-    sleep 1
+    sleep 1.5
     ui_print "• And you can manually clear cache"
     ui_print "• By typing 'su -c cleaner' in Terminal."
     ui_print ""
-    sleep 1
+    sleep 1.5
     ui_print "• More info about this module please read on cacheplus official repository."
     ui_print "• https://github.com/kageyuki-takeru/cacheplus"
     ui_print ""
-    sleep 1
+    sleep 1.5
 }
 
 EXEC() {
     ui_print "Installing..."
-    sleep 0.7
+    sleep 1
     ui_print "- Extracting module files"
     unzip -o "$ZIPFILE" 'system/*' -d $MODPATH >&2
     unzip -o "$ZIPFILE" module.prop -d $MODPATH >&2
     unzip -o "$ZIPFILE" service.sh -d $MODPATH >&2
     chmod -R 755 $MODPATH/system/xbin
-    sleep 0.7
+    sleep 1
     
     ui_print "- Set permissions"
     set_perm_recursive $MODPATH 0 0 0755 0644
     set_perm $MODPATH/system/xbin/cleaner 0 0 0755 0755
-    sleep 0.7
+    sleep 1
 }
 
 if [ ! "$SKIPUNZIP" = "0" ]; then
